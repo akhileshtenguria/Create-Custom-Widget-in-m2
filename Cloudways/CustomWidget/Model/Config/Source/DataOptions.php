ec826229ad389a2dd8263bcc4771852c4ce5c336
akhileshtenguria <79194761+akhileshtenguria@users.noreply.github.com>
2022-05-31 12:36:44 +0530

    Create Custom Widget in m2
    Create Custom Widget in m2

---
+++ Cloudways/CustomWidget/Model/Config/Source/DataOptions.php
@@ -0,0 +1,27 @@
+<?php
+namespace Cloudways\CustomWidget\Model\Config\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+class DataOptions implements ArrayInterface
+{
+   public function toOptionArray()
+   {
+       $options = [
+           [
+               'value' => 'option1',
+               'label' => __('Option-1')
+           ],
+           [
+               'value' => 'option2',
+               'label' => __('Option-2')
+           ],
+           [
+               'value' => 'option3',
+               'label' => __('Option-3')
+           ],
+       ];
+
+       return $options;
+   }
+}
